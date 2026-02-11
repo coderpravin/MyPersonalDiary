1a10910acaa1c8efd7bbcb2a123cf160f5386e3e
coderpravin <impravin00@gmail.com>
2026-02-11 16:28:53 +0530

    installed dj-database-url for render database

--- mydiary_project/settings.py
+++ mydiary_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_utl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,7 +73,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mydiary_project.wsgi.application'
 
 
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
+}
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -83,7 +90,7 @@ DATABASES = {
         'PORT':5432
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
